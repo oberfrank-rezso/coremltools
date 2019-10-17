4daa597d2996495077a845d774f321a474dadc07
Oberfrank Rezső <oberfrank@protonmail.com>
2019-10-17 18:25:44 +0200

    add tf.keras support for 1.14.0

--- coremltools/converters/keras/__init__.py
+++ coremltools/converters/keras/__init__.py
@@ -5,6 +5,7 @@
 
 from ..._deps import HAS_KERAS_TF as _HAS_KERAS_TF
 from ..._deps import HAS_KERAS2_TF as _HAS_KERAS2_TF
+from ..._deps import HAS_TF_1_14 as HAS_TF_1_14
 
 if _HAS_KERAS_TF or _HAS_KERAS2_TF:
     import keras as _keras
@@ -14,6 +15,12 @@ if _HAS_KERAS_TF or _HAS_KERAS2_TF:
         _HAS_KERAS_TF = False
         _HAS_KERAS2_TF = False
         _logging.warn('Currently, only Keras models with TensorFlow backend can be converted to CoreML.')
+
+if HAS_TF_1_14:
+    import tensorflow.python.keras as _keras
+    import logging as _logging
+    from ._kerastf_converter import convert, convertToSpec
+    print('Using tensorflow.python.keras as keras.')
 
 # if _HAS_KERAS_TF:
 #     from ._keras_converter import convert
